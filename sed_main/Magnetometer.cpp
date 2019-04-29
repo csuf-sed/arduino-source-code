2ba923f0da7fb0ec8d022719655a3a5382d19643
rdsoliven06 <48046741+rdsoliven06@users.noreply.github.com>
2019-04-29 16:17:52 -0700

    Update Magnetometer.cpp

--- sed_main/Magnetometer.cpp
+++ sed_main/Magnetometer.cpp
@@ -1,9 +1,9 @@
 #include "Magnetometer.h"
 
 Magnetometer magnetometer;
-
+int status;
 void Magnetometer::begin() {
-  IMU.begin();
+  status = IMU.begin();
   IMU.setMagCalX(hxb,hxs);  
   IMU.setMagCalY(hyb,hys);
 }
@@ -12,7 +12,7 @@ float Magnetometer::getDegrees() {
   float heading = atan2(IMU.getMagX_uT(),IMU.getMagY_uT());
   if (heading < 0)      heading += 2 * PI;
   if (heading > 2 * PI) heading -= 2 * PI;
-  return (heading * 180 / PI) - 90 - 5;
+  return ((static_cast<int>(heading * 180 / PI)) - 90 + 360)%360;
 }
 
 float Magnetometer::getRadians() {
